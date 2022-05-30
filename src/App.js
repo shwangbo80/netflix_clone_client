ce931cd9cc54157839f75b59f43cd80a7bc05f83
Soo Hwangbo <soohwangbo@Soos-MacBook-Pro.local>
2022-05-30 14:18:55 -0700

    fixing page not found error on netlify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, HashRouter} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./Components/Home";
 import Movies from "./Components/Movies";
@@ -11,11 +11,11 @@ import "./App.css";
 
 export default function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/movies" element={<Movies />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
