16590b114877c70ed5c7a225941b340204a5e1f3
Soo Hwangbo <soohwangbo@Soos-MacBook-Pro.local>
2022-05-30 06:44:02 -0700

    first commit

--- src/GenreComponents/NavbarComponent.js
+++ src/GenreComponents/NavbarComponent.js
@@ -1,30 +1,24 @@
-import React from "react"
-import {Container, Navbar, Nav} from "react-bootstrap"
+import React from "react";
+import {Container, Navbar, Nav} from "react-bootstrap";
 
 export default function NavbarComponent() {
-  return (
-    <>
-      <Navbar variant="dark" className="navbar">
-        <Container fluid>
-          <Navbar.Brand href="/movies">
-            <p className="logoText">SOOFLIX</p>
-          </Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home" className="navText">
-              Movies
-            </Nav.Link>
-            <Nav.Link href="#features" className="navText">
-              Dramas
-            </Nav.Link>
-            <Nav.Link href="#pricing" className="navText">
-              About
-            </Nav.Link>
-            <Nav.Link href="/" className="navText">
-              Logout
-            </Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
-  )
+    return (
+        <>
+            <Navbar variant="dark" className="navbar">
+                <Container fluid>
+                    <Navbar.Brand href="/movies">
+                        <p className="logoText">SOOFLIX</p>
+                    </Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#pricing" className="navText">
+                            About
+                        </Nav.Link>
+                        <Nav.Link href="/" className="navText">
+                            Logout
+                        </Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
+    );
 }
--- src/Movies.js
+++ src/Movies.js
@@ -1,43 +1,42 @@
-import {React, useState, useEffect} from "react"
-import {Container} from "react-bootstrap"
-import axios from "axios"
+import {React, useState, useEffect} from "react";
+import {Container} from "react-bootstrap";
+import axios from "axios";
 
-import FeaturedComponent from "./GenreComponents/FeaturedComponent"
-import MartialArtsComponent from "./GenreComponents/MartialArtsComponent"
-import AnimeComponent from "./GenreComponents/AnimeComponent"
-import DocumentaryComponent from "./GenreComponents/DocumentaryComponent"
-import SportsComponent from "./GenreComponents/SportsComponent"
-import UfcComponent from "./GenreComponents/UfcComponent"
-import WweComponent from "./GenreComponents/WweComponent"
-import NavbarComponent from "./GenreComponents/NavbarComponent"
-import FooterComponent from "./GenreComponents/FooterComponent"
+import FeaturedComponent from "./GenreComponents/FeaturedComponent";
+import MartialArtsComponent from "./GenreComponents/MartialArtsComponent";
+import AnimeComponent from "./GenreComponents/AnimeComponent";
+import DocumentaryComponent from "./GenreComponents/DocumentaryComponent";
+import SportsComponent from "./GenreComponents/SportsComponent";
+import UfcComponent from "./GenreComponents/UfcComponent";
+import WweComponent from "./GenreComponents/WweComponent";
+import NavbarComponent from "./GenreComponents/NavbarComponent";
+import FooterComponent from "./GenreComponents/FooterComponent";
 
 export default function Movies() {
-  const [apiData, setApiData] = useState([])
+    const [apiData, setApiData] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get("http://127.0.0.1:8000/api/movies/")
-      .then((response) => setApiData(response.data))
-      .catch((error) => {
-        setApiData({errorMessage: error.message})
-        console.error("There was an error!", error)
-      })
-  }, [])
+    useEffect(() => {
+        axios
+            .get("http://localhost:8000/movies/")
+            .then((response) => setApiData(response.data))
+            .catch((error) => {
+                setApiData({errorMessage: error.message});
+                console.error("There was an error!", error);
+            });
+    }, []);
 
-  return (
-    <div>
-      <Container fluid className="p-0">
-        <NavbarComponent />
-        <FeaturedComponent apiData={apiData} />
-        <MartialArtsComponent apiData={apiData} />
-        <AnimeComponent apiData={apiData} />
-        <DocumentaryComponent apiData={apiData} />
-        <SportsComponent apiData={apiData} />
-        <UfcComponent apiData={apiData} />
-        <WweComponent apiData={apiData} />
-        <FooterComponent />
-      </Container>
-    </div>
-  )
+    return (
+        <div>
+            <Container fluid className="p-0">
+                <NavbarComponent />
+                <AnimeComponent apiData={apiData} />
+                <MartialArtsComponent apiData={apiData} />
+                <DocumentaryComponent apiData={apiData} />
+                <SportsComponent apiData={apiData} />
+                <UfcComponent apiData={apiData} />
+                <WweComponent apiData={apiData} />
+                <FooterComponent />
+            </Container>
+        </div>
+    );
 }
